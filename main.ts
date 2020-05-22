50282b687fa503d95c6f2276eb11448aabf6414c
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-05-22 12:24:00 +0100

    refacts: unnecesary bootstrap func

--- main.ts
+++ main.ts
@@ -1,21 +1,18 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
 import { router } from './routes.ts';
 
-async function bootstrap() {
-  const env = Deno.env.toObject();
-  const PORT = env.PORT || 3000;
-  const HOST = env.HOST || '127.0.0.1';
-  
-  const app = new Application();
-  
-  app.use(
-    router.routes(),
-    router.allowedMethods()
-  );
-  
-  console.log(`Listening on port ${PORT}`);
-  
-  await app.listen(`${HOST}:${PORT}`);
-}
+// bootstrap()
+const env = Deno.env.toObject();
+const PORT = env.PORT || 3000;
+const HOST = env.HOST || '127.0.0.1';
 
-bootstrap();
+const app = new Application();
+
+app.use(
+  router.routes(),
+  router.allowedMethods()
+);
+
+console.log(`Listening on port ${PORT}`);
+
+await app.listen(`${HOST}:${PORT}`);
